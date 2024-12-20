9ca33b4153b04b9c7c3797708b2859c6ed4e3f12
Kandaswamy Palaniappan <meenakanda2012@gmail.com>
2024-12-19 22:23:10 -0800

    updated fix for chrome111.x

--- src/main/java/com/qa/ecommerce/factory/OptionsManager.java
+++ src/main/java/com/qa/ecommerce/factory/OptionsManager.java
@@ -18,6 +18,7 @@ public class OptionsManager {
 	
 	public ChromeOptions getChromeOption() {
 		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
 		if(Boolean.parseBoolean(prop.getProperty("headless"))) co.addArguments("--headless");
 		if(Boolean.parseBoolean(prop.getProperty("incognito"))) co.addArguments("--incognito");
 		return co;		
